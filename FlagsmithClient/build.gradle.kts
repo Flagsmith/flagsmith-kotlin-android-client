ed06768b1444460f4cf7101fd34ae14243ae4b01
Robert Valdes <rob.valdes@hotmail.com>
2023-01-02 23:15:00 +0000

    Improve gradle task test reporting

--- FlagsmithClient/build.gradle.kts
+++ FlagsmithClient/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("com.android.library")
     kotlin("android")
@@ -48,6 +51,45 @@ dependencies {
     testImplementation("org.mock-server:mockserver-netty-no-dependencies:5.14.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+}
+
+tasks.withType(Test::class) {
+    testLogging {
+        events(
+            TestLogEvent.FAILED,
+            TestLogEvent.PASSED,
+            TestLogEvent.SKIPPED,
+            TestLogEvent.STANDARD_OUT
+        )
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+        showStandardStreams = true
+        exceptionFormat = TestExceptionFormat.FULL
+
+        debug {
+            events(
+                TestLogEvent.STARTED,
+                TestLogEvent.FAILED,
+                TestLogEvent.PASSED,
+                TestLogEvent.SKIPPED,
+                TestLogEvent.STANDARD_ERROR,
+                TestLogEvent.STANDARD_OUT
+            )
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+        info.events = debug.events
+        info.exceptionFormat = debug.exceptionFormat
+
+        afterSuite(KotlinClosure2({ desc: TestDescriptor, result: TestResult ->
+            if (desc.parent == null) {
+                val output =
+                    "|  Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} passed, ${result.failedTestCount} failed, ${result.skippedTestCount} skipped)  |"
+                val repeatLength = output.length
+                println("\n${"-".repeat(repeatLength)}\n$output\n${"-".repeat(repeatLength)}")
+            }
+        }))
+    }
 }
 
 publishing {
