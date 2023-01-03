7ec7e62f36b5d93620e629f95ab53745e03c1538
Robert Valdes <rob.valdes@hotmail.com>
2023-01-03 23:54:41 +0000

    Bump plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application").version("7.3.0").apply(false)
-    id("com.android.library").version("7.3.0").apply(false)
-    kotlin("android").version("1.7.20").apply(false)
+    id("com.android.application").version("7.3.1").apply(false)
+    id("com.android.library").version("7.3.1").apply(false)
+    kotlin("android").version("1.8.0").apply(false)
 }
 
 val clean by tasks.registering(Delete::class) {
