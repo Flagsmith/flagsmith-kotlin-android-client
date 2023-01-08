e2dfd7e0b3097f2c6f75f4dab6d08c96e863ae01
Robert Valdes <rob.valdes@hotmail.com>
2023-01-08 18:17:21 +0000

    Add kover to report code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application").version("7.3.1").apply(false)
     id("com.android.library").version("7.3.1").apply(false)
     kotlin("android").version("1.8.0").apply(false)
+    id("org.jetbrains.kotlinx.kover").version("0.6.1").apply(false)
 }
 
 val clean by tasks.registering(Delete::class) {
