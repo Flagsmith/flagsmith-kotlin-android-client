b182618bcb25c9cc0a4d1bff70528c0b5289bd8f
Robert Valdes <rob.valdes@hotmail.com>
2023-01-08 22:15:37 +0000

    Upload merged xml report and generate code coverage comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,11 @@ plugins {
     id("com.android.application").version("7.3.1").apply(false)
     id("com.android.library").version("7.3.1").apply(false)
     kotlin("android").version("1.8.0").apply(false)
-    id("org.jetbrains.kotlinx.kover").version("0.6.1").apply(false)
+    id("org.jetbrains.kotlinx.kover").version("0.6.1")
+}
+
+koverMerged {
+    enable()
 }
 
 val clean by tasks.registering(Delete::class) {
