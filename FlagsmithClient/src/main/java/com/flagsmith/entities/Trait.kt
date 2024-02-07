aa8ecda01adaef393043cb46765ee43cb12452a3
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-02-07 14:39:21 +0000

    Allow non-string trait values

--- FlagsmithClient/src/main/java/com/flagsmith/entities/Trait.kt
+++ FlagsmithClient/src/main/java/com/flagsmith/entities/Trait.kt
@@ -6,11 +6,11 @@ import com.google.gson.annotations.SerializedName
 data class Trait(
     val identifier: String? = null,
     @SerializedName(value = "trait_key") val key: String,
-    @SerializedName(value = "trait_value") val value: String
+    @SerializedName(value = "trait_value") val value: dynamic
 )
 
 data class TraitWithIdentity(
     @SerializedName(value = "trait_key") val key: String,
-    @SerializedName(value = "trait_value") val value: String,
+    @SerializedName(value = "trait_value") val value: dynamic,
     val identity: Identity
 )
