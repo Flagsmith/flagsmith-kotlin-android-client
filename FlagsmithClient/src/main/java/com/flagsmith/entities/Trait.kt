12f7d0a326dfc37d70cada9f1c410e4097d6a7ed
Gareth Reese <gareth@foresightmobile.com>
2024-02-12 09:17:58 +0000

    Allow non-string trait types

--- FlagsmithClient/src/main/java/com/flagsmith/entities/Trait.kt
+++ FlagsmithClient/src/main/java/com/flagsmith/entities/Trait.kt
@@ -7,10 +7,18 @@ data class Trait(
     val identifier: String? = null,
     @SerializedName(value = "trait_key") val key: String,
     @SerializedName(value = "trait_value") val value: String
-)
+) {
+    constructor(key: String, value: String) : this(null, key, value)
+}
 
 data class TraitWithIdentity(
     @SerializedName(value = "trait_key") val key: String,
-    @SerializedName(value = "trait_value") val value: String,
-    val identity: Identity
-)
+    @SerializedName(value = "trait_value") val value: Any,
+    val identity: Identity,
+) {
+    // Add a constructor that takes a string and sets the value to it
+    constructor(key: String, value: String, identity: Identity) : this(key, value as Any, identity)
+
+    // Add a constructor that takes an int and sets the value to it
+    constructor(key: String, value: Int, identity: Identity) : this(key, value as Any, identity)
+}
